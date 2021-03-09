7c1a7971e4147019baede8114039d5f9536fc4a9
John <73211231+01687961788@users.noreply.github.com>
2021-03-09 23:22:29 +0700

    Add files via upload

---
+++ ASG3/ProductLibrary/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductLibrary
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public String ProductName { get; set; }
+        public int Quantity { get; set; }
+        public float Unitprice { get; set; }
+        public float SubTotal
+        {
+            get
+            {
+                return Unitprice * Quantity;
+            }
+        }
+
+    }
+}
---
+++ ASG3/ProductLibrary/ProductDB.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductLibrary
+{
+    public class ProductDB
+    {
+        string strConnection;
+        public ProductDB()
+        {
+            strConnection = getConnectionString();
+        }
+        private string getConnectionString()
+        {
+            string strConnection = "Server=SE140040\\SQLEXPRESS;database=SaleDB;uid=sa;pwd=01687961788hieu";
+            return strConnection;
+        }
+
+        bool AddNewProduct(Product p)
+        {
+            return false;
+        }
+        bool RemoveProduct(Product p)
+        {
+            
+            return false;
+        }
+        bool UpdateProduct(Product p)
+        {
+            
+            return false;
+        }
+        
+    }
+}
